c13ca8606e281d2fbb35f2cedede2c2313aabe00
shoyonp <shoyonkumar143@gmail.com>
2024-10-28 03:53:34 +0600

    added spinner

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,32 +1,70 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
+import {
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import { Audio, DNA } from "react-loader-spinner";
 
 const Phones = () => {
   const [phones, setPhones] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     // fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
     //   .then((res) => res.json())
     //   .then((data) => setPhones(data.data));
 
-    axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
-    .then(data => {
+    axios
+      .get("https://openapi.programming-hero.com/api/phones?search=iphone")
+      .then((data) => {
         const phoneData = data.data.data;
-        const phonesWithFakeData = phoneData.map(phone => {
-            const obj ={
-                name: phone.phone_name,
-                price: parseInt(phone.slug.split('-')[1])
-            }
-            return obj;
-        })
+        const phonesWithFakeData = phoneData.map((phone) => {
+          const obj = {
+            name: phone.phone_name,
+            price: parseInt(phone.slug.split("-")[1]),
+          };
+          return obj;
+        });
         setPhones(phonesWithFakeData);
-    });
+        setLoading(false);
+      });
   }, []);
 
   return (
     <div>
-      <h2 className="text-5xl font-bold text-white my-5"> Phones:{phones.length}</h2>
+      {loading && (
+        <div>
+          <Audio
+            height="80"
+            width="80"
+            radius="9"
+            color="green"
+            ariaLabel="three-dots-loading"
+            wrapperStyle
+            wrapperClass
+          />
+          <DNA
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="dna-loading"
+            wrapperStyle={{}}
+            wrapperClass="dna-wrapper"
+          />
+        </div>
+      )}
+
+      <h2 className="text-5xl font-bold text-white my-5">
+        {" "}
+        Phones:{phones.length}
+      </h2>
       <BarChart width={1000} height={400} data={phones}>
         <Bar dataKey="price" fill="#8884d8"></Bar>
         <XAxis dataKey="name"></XAxis>
