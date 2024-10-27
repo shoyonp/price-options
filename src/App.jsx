f44f03db786b5bd3ecb890ae75847bac81831173
shoyonp <shoyonkumar143@gmail.com>
2024-10-28 00:20:54 +0600

    added li

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+// import DaisyNav from "./components/DaisyNav/DaisyNav";
 
 function App() {
   return (
     <>
+    {/* <DaisyNav></DaisyNav> */}
+    <NavBar></NavBar>
       <h1 className="text-7xl bg-emerald-500">Vite + React</h1>
 
     </>
---
+++ src/components/Link/Link.jsx
@@ -0,0 +1,10 @@
+const Link = ({route}) => {
+  return (
+    <li className="mr-10">
+      <a href={route.path}>{route.name}</a>
+    </li>
+  );
+};
+
+// Link.propTypes =
+export default Link;
