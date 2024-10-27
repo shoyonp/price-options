dd7169b960e03172da1ebba19d9c82f157bfa943
shoyonp <shoyonkumar143@gmail.com>
2024-10-28 01:55:09 +0600

    card done

---
+++ src/components/Feature/Feature.jsx
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types";
+import { FaCheckCircle } from "react-icons/fa";
+
+const Feature = ({ feature }) => {
+  return <div>
+    <p className="flex items-center gap-3"><FaCheckCircle className=""></FaCheckCircle> {feature}</p>
+  </div>;
+};
+
+Feature.propTypes = {
+  feature: PropTypes.string,
+};
+export default Feature;
--- src/components/PriceOptions/PriceOptions.jsx
+++ src/components/PriceOptions/PriceOptions.jsx
@@ -78,11 +78,13 @@ const PriceOptions = () => {
       
 
     return (
-        <div>
+        <div className="m-12">
             <h2 className="text-5xl">Best prices in the town</h2>
-            {
+           <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-3">
+           {
                 priceOptions.map(option => <PriceOption key={option.id} option={option}></PriceOption>)
             }
+           </div>
         </div>
     );
 };
