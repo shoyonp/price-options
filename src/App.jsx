09cc2c6f7e3297957c3bb5491b6d1e1680e3c755
shoyonp <shoyonkumar143@gmail.com>
2024-10-28 03:11:12 +0600

    added bar chart

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import DaisyNav from "./components/DaisyNav/DaisyNav";
 import NavBar from "./components/NavBar/NavBar";
 import PriceOptions from "./components/PriceOptions/PriceOptions";
 import LineChart from "./components/LineChart/LineChart";
+import Phones from "./components/Phones/Phones";
 // import DaisyNav from "./components/DaisyNav/DaisyNav";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <DaisyNav></DaisyNav>
       <PriceOptions></PriceOptions>
       <LineChart></LineChart>
+      <Phones></Phones>
     </>
   );
 }
